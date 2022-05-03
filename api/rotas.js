d493a94048db41e856c8ecac0dc15bff67ed64f3
Pecista - Distribuição e Representação de Autopeças <tecnologia.pecista@gmail.com>
2022-05-03 10:08:57 -0300

    commit "file vercel"

--- api/rotas.js
+++ api/rotas.js
@@ -1,18 +1,16 @@
 const express = require("express");
-const router = express.Router();
 const app = express();
 
 
 app.use(express.json());
 
+const users = [];
 
-const user = [];
-
-router.get("/", async (req, res) => {
+app.get("/", (req, res) => {
     try {
         res.json({
             status: 200,
-            message: ["Api", "Pecista/Kaizen", "Version 1.0"],
+            message: {"Api": "Pecista/Kaizen", "Version": "1.0"}
         });
     } catch (error) {
         console.error(error);
@@ -20,14 +18,35 @@ router.get("/", async (req, res) => {
     }
 })
 
-router.post("/user", async (req, res) => {
-    try{
-        //await user.create(dados);
-    console.log(req.body)
-    } catch  (error) {
-        console.error(error);
-        return res.status(500).send("Server error");
-    }
+app.post("/user",  (req, res) => {
+
+        // const event = req.body;
+
+        // if( event.activity_type != "route-optimized") {
+        //     res.status(404).json({
+        //         error: true,
+        //         message: "requisição invalida"
+        //     });
+        // } else {
+        //     res.status(200).json({
+        //         error: false,
+        //         message: "requisição valida"
+        //     });
+        // }
+    const { route_id, activity_type } = req.body;
+
+    const user = {
+        route_id,
+        activity_type
+    };
+
+
+
+    users.push(user);
+
+
+
+    return res.status(201).json({error: false, message: "Resiquição via POST"});
 }); 
 
-module.exports = router;
+module.exports = app;
