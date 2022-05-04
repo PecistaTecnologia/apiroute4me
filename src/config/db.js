c2078036f22ba7beecb4d6315b29f390822fd8e6
Pecista - Distribuição e Representação de Autopeças <tecnologia.pecista@gmail.com>
2022-05-04 11:46:42 -0300

    file edition

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,42 @@
-const mysql = require('mysql2/promise');
 
-var pool = mysql.createPool({
-  host: '162.241.62.177',
-  user: 'pain1521_pecista',
-  password: ['@Pecista2021'],
-  database: 'pain1521_rastreio'
-});
-
-  try{
-      console.log("Conexão com MySQL realizado com sucesso");
+// Require and initialize outside of your main handler
+const mysql = require('serverless-mysql')({
+  config: {
+    host     : process.env.MYSQL_HOST,
+    database : process.env.MYSQL_DATASABE,
+    user     : process.env.MYSQL_USERNAME,
+    password : process.env.MYSQL_PASSWORD
   }
-  catch{
-      console.log(err)
-  }
+})
+
+
+// Main handler function
+async function handler(event, context) {
+//  Run your query
+ let results = await mysql.query('SELECT * FROM r4m_rotas')
+
+//  Run clean up function
+ await mysql.end()
+
+//  Return the results
+ return results
+}
 
 
 
-module.exports = pool;
 
+// const mysql = require('mysql2/promise');
+
+// var pool = mysql.createPool({
+//   host: '162.241.62.177',
+//   user: 'pain1521_pecista',
+//   password: ['@Pecista2021'],
+//   database: 'pain1521_rastreio'
+// });
+
+//   try{
+//       console.log("Conexão com MySQL realizado com sucesso");
+//   }
+//   catch{
+//       console.log(err)
+//   }
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const db = require('./Config/db');
+require('./Config/db');
 
 
 app.use(express.json());
