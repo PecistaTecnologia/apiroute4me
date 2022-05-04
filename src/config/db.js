4d65b1602552a50bf3d1d110e907d61cd2ebd98e
Pecista - Distribuição e Representação de Autopeças <tecnologia.pecista@gmail.com>
2022-05-04 11:55:50 -0300

    file vercel

--- src/config/db.js
+++ src/config/db.js
@@ -1,42 +1,19 @@
+const mysql = require('mysql2/promise');
 
-// Require and initialize outside of your main handler
-const mysql = require('serverless-mysql')({
-  config: {
-    host     : process.env.MYSQL_HOST,
-    database : process.env.MYSQL_DATASABE,
-    user     : process.env.MYSQL_USERNAME,
-    password : process.env.MYSQL_PASSWORD
+
+var pool = mysql.createPool({
+  host : '162.241.62.177',
+  user: 'pain1521_pecista',
+  password: ['@Pecista2021'],
+  database: 'pain1521_rastreio'
+});
+
+  try{
+      console.log("Conexão com MySQL realizado com sucesso");
   }
-})
+  catch{
+      console.log(err)
+  }
 
+module.exports = pool;
 
-// Main handler function
-async function handler(event, context) {
-//  Run your query
- let results = await mysql.query('SELECT * FROM r4m_rotas')
-
-//  Run clean up function
- await mysql.end()
-
-//  Return the results
- return results
-}
-
-
-
-
-// const mysql = require('mysql2/promise');
-
-// var pool = mysql.createPool({
-//   host: '162.241.62.177',
-//   user: 'pain1521_pecista',
-//   password: ['@Pecista2021'],
-//   database: 'pain1521_rastreio'
-// });
-
-//   try{
-//       console.log("Conexão com MySQL realizado com sucesso");
-//   }
-//   catch{
-//       console.log(err)
-//   }
