e51cceb5376459071dd8a126dd783347680b350b
Pecista - Distribuição e Representação de Autopeças <tecnologia.pecista@gmail.com>
2022-05-11 15:59:40 -0300

    connection 'server'

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const User = require('./src/routes');
 const app = express();
 
 
-const PORT  = process.env.PORT || 5050;
-
 app.use("/", User);
 
-app.listen(PORT, () => console.log(`Server rodando na porta  ${PORT}`));
+app.listen(3333, () => console.log(`Server rodando na porta  ${3333}`));
 
 
